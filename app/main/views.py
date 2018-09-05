ac94f2b7eca14b8386b0bd5c78601384948c7dfc
Eddie956 <mutugieddie3@gmail.com>
2018-09-05 16:31:16 +0300

    commit views changes on pitch id

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,16 @@
 from flask import render_template, request, redirect, url_for
 from main import main
 from .forms import PitchForm
+from ..models import Pitch, Comment,Category
+
+@main.route('/pitch/<pitch:id>', methods = ['GET', 'POST'])
+
+def pitch(id):
+    '''
+    Function the returns pitch for comment to be added
+    '''
+    pitch =Pitch.query.get(id)
+    comment = Comment.get_comments(pitch_id=id)
+
+    title = f'Pitch { pitch.id }'
+    return render_template('pitch.html', title=title, pitch=pitch, comment=comment)
