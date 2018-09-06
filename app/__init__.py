23e2729e29fe7d7613a0be96e544bbe202752248
Eddie956 <mutugieddie3@gmail.com>
2018-09-06 16:01:38 +0300

    called the init app method and passed in the app instance imported a SQLAchemy and created a db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
 from config import config_options,DevConfig
 
 bootstrap = Bootstrap()
@@ -14,6 +16,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
