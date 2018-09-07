b33faecc72a71edc83e9e95394281fc0dbcb6507
Eddie956 <mutugieddie3@gmail.com>
2018-09-07 11:13:58 +0300

    import the Login Manager class from the flask-login extension then create an instance of the class.
    login_manager.session_protection attribute provides different security levels and by setting it to strong will monitor the changes in a user's request header and log the user out
    
    deleted class role

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
 
     @property
@@ -19,18 +18,9 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.pass_secure, password)
-        
+
     def __repr__(self):
         return f'User {self.username}'
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User', backref='role', lazy="dynamic")
-
-
-    def __repr__(self):
-        return f'User {self.name}'
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 
-from app.models import User,Role
+from app.models import User
 
 
 #Creating app instance 
@@ -18,7 +18,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role = Role)
+    return dict(app=app, db=db, User=User)
 
 
 
