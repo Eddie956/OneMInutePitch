b190a5f6ae0c78dc3548cf95dea850a800c31821
Eddie956 <mutugieddie3@gmail.com>
2018-09-08 08:56:44 +0300

    import the photos instance from our application factory module. We then create a route that will process our form submission request. This route only accepts post requests. then query the database to pick a user with the same username we passed in. We then use the flask request function to check if any parameter with the name photo has been passed into the request. use the save method to save the file into our application. We then create a path variable to where the file is stored. We then update the profile_pic_path property in our user table and store the path to the file. finally commit the changes to the database and redirect the user back to the profile page.

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from .forms import PitchForm, UpdateProfile,ContentForm,CommentForm
 from flask_login import login_required
 from ..models import  User
-from .. import db
+from .. import db,photos
 
 
 
@@ -39,6 +39,16 @@ def update_profile(uname):
     return render_template('profile/update.html', form=form)
 
 
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
 @main.route('/new/pitch')
 @login_required
 def new_pitch():
