81373cc5398124b9e59671b2b259ed092d7ca2aa
Eddie956 <mutugieddie3@gmail.com>
2018-09-08 09:09:38 +0300

    etup the SMTP server and configure the port to use the gmail SMTP server port. Then we set MAIL_USE_TLS configuration to true which enables a transport layer security to secure the emails when sending the emails.

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ from flask import Flask
 class Config:
     SECRET_KEY = 'grESrtgb284gvfnfd58437bhb'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get"mutugieddie3@gmail.com"
+    MAIL_PASSWORD = os.environ.get"winnieeddie321"
 class ProdConfig(Config):
     pass
 
